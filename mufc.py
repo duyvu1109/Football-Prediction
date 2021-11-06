6d239716a9085df84fda719568de290b6d8eaa85
duyvu1109 <duyvu1109@gmail.com>
2021-11-06 23:27:27 +0700

    crawl done

--- mufc.py
+++ mufc.py
@@ -5,10 +5,32 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from plotting import show1Chart
+from plotting import show1Chart, showCharts
 # Open Microsoft Edge 
 driver = webdriver.Edge(executable_path='msedgedriver.exe')
 driver.get("https://www.11v11.com/teams/manchester-united/tab/matches/")
+
+_win, _draw, _lose = [], [], []
+
+### Season 2021-2022
+win, draw, lose = 0,0,0
+result = driver.find_elements_by_xpath('//*[@id="pageContent"]/div[2]/table/tbody/tr/td[3]/span')
+for r in result:
+    if (r.text == 'W'):
+        win+=1
+    elif (r.text == 'D'):
+        draw+=1
+    else:
+        lose+=1
+_win.append(win)
+_draw.append(draw)
+_lose.append(lose)
+print('21-22: win: {0}, draw: {1}, lose: {2}'.format(win, draw, lose))
+
+sleep(2)
+
+season2021 = driver.find_element_by_xpath('//*[@id="season"]/li[2]/a')
+season2021.click()
 
 win, draw, lose = 0,0,0
 result = driver.find_elements_by_xpath('//*[@id="pageContent"]/div[2]/table/tbody/tr/td[3]/span')
@@ -19,10 +41,41 @@ for r in result:
         draw+=1
     else:
         lose+=1
-    
-print('win: {0}, draw: {1}, lose: {2}'.format(win, draw, lose))
+
+_win.append(win)
+_draw.append(draw)
+_lose.append(lose)
+print('20-21: win: {0}, draw: {1}, lose: {2}'.format(win, draw, lose))
+
+sleep(2)
+
+season2020 = driver.find_element_by_xpath('//*[@id="season"]/li[3]/a')
+season2020.click()
+
+win, draw, lose = 0,0,0
+result = driver.find_elements_by_xpath('//*[@id="pageContent"]/div[2]/table/tbody/tr/td[3]/span')
+for r in result:
+    if (r.text == 'W'):
+        win+=1
+    elif (r.text == 'D'):
+        draw+=1
+    else:
+        lose+=1
+
+_win.append(win)
+_draw.append(draw)
+_lose.append(lose)
+print('19-20: win: {0}, draw: {1}, lose: {2}'.format(win, draw, lose))
+
+
+
+
+
 driver.close()
-show1Chart(win, draw, lose)
+showCharts(_win, _draw, _lose)
 
 sleep(10)
+driver.close()
+# show1Chart(win, draw, lose)
+
 
--- plotting.py
+++ plotting.py
@@ -1,7 +1,23 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 def show1Chart(win, draw, lose):
     df = pd.DataFrame([['win','c1',win],['draw','c1',draw],['lose','c1',lose]],columns=['detail','season','val'])
     df.pivot("season", "detail", "val").plot(kind='bar')
+    plt.show()
+
+
+
+def showCharts(_win, _draw, _lose):
+    x = np.arange(3)
+    width = 0.2
+    # plot data in grouped manner of bar type
+    plt.bar(x-0.2, _win, width, color='cyan')
+    plt.bar(x, _draw, width, color='orange')
+    plt.bar(x+0.2, _lose, width, color='green')
+    plt.xticks(x, ['21-22', '20-21', '19-20'])
+    plt.xlabel("Season")
+    plt.ylabel("Match")
+    plt.legend(["WIN", "DRAW", "LOSE"])
     plt.show()
