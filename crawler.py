e619e39da61fcb39fadc85194427eca84cfe9b2a
duyvu1109 <duyvu1109@gmail.com>
2021-11-08 23:51:08 +0700

    crawl data for season 21 22

---
+++ crawler.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from plotting import show1Chart, showCharts
+import pandas as pd
+# Open Microsoft Edge 
+driver = webdriver.Edge(executable_path='msedgedriver.exe')
+driver.get("https://www.11v11.com/teams/manchester-united/tab/matches/")
+
+### Season 2021-2022
+
+matchResult = []
+matchList = driver.find_elements_by_xpath('//*[@id="pageContent"]/div[2]/table/tbody/tr/td[3]/span')
+for entry in matchList:
+    matchResult.append(entry.text)
+
+matchName = []
+matchList = driver.find_elements_by_xpath('//*[@id="pageContent"]/div[2]/table/tbody/tr/td[2]/a')
+for entry in matchList:
+    matchName.append(entry.text)
+
+matchScore = []
+matchList = driver.find_elements_by_xpath('//*[@id="pageContent"]/div[2]/table/tbody/tr/td[4]')
+for entry in matchList:
+    matchScore.append(entry.text)
+
+matchHA = []
+for match in matchName:
+    if (str(match)[0:17] == 'Manchester United'):
+        matchHA.append('H')
+    else:
+        matchHA.append('A')
+
+df = pd.DataFrame({'Match':matchName,
+                    'Result':matchResult,
+                    'Score': matchScore,
+                    'Home-Away': matchHA})
+
+df.to_excel('data.xlsx', sheet_name='season21_22')
+
+print('Done!')
+
+driver.close()
